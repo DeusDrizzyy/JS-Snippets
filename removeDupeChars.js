f3e9943170bbc3fc8ed2b05a4002f9e9c14a0c5d
DeusDrizzyy <66096216+DeusDrizzyy@users.noreply.github.com>
2022-02-24 11:53:28 -0300

    Create removeDupeChars.js

---
+++ removeDupeChars.js
@@ -0,0 +1,8 @@
+function removeDupeChars(e, r = !1) {
+    return e.split("").filter((e, i, n) => n[i] !== n[r ? i + 2 : i + 1]).join("");
+}
+
+/* Example Outputs: */
+removeDupeChars('Hellooooooo, how are youuuuuuuu?????') // Hello, how are you?????
+removeDupeChars('Wow, that code is really short!', true) // Wow, that code is really short!
+removeDupeChars('Ssssshhhhh, be quiet please!') // Ssh, be quiet please!
