94920b7cfcd199879008e3cf1f48834a2a47db6d
DeusDrizzyy <gameplayit17@gmail.com>
2022-02-25 15:35:05 -0300

    Update checkArrayValues.js

--- checkArrayValues.js
+++ checkArrayValues.js
@@ -2,6 +2,8 @@
 const includesAny = (arr, values) => values.some(v => arr.includes(v));
 /* Checks if all the elements in values are included in arr. */
 const includesAll = (arr, values) => values.every(v => arr.includes(v));
+/* Checks if all elements in an array are equal. */
+const allEqual = arr => arr.every(val => val === arr[0]);
 
 /* Example Outputs: */
 includesAny([1, 2, 3, 4], [2, 9]); // true
@@ -9,3 +11,6 @@ includesAny([1, 2, 3, 4], [8, 9]); // false
 
 includesAll([1, 2, 3, 4], [1, 4]); // true
 includesAll([1, 2, 3, 4], [1, 5]); // false
+
+allEqual([1, 1, 1, 1]); // true
+allEqual([1, 2, 3, 4, 5, 6]); // false
