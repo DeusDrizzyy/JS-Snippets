b853f46d72287f696cd176ca568e6a98e5ee5e53
DeusDrizzyy <gameplayit17@gmail.com>
2022-02-25 19:11:13 -0300

    Update didYouMean.js

--- didYouMean.js
+++ didYouMean.js
@@ -1,4 +1,4 @@
-function didYouMean(e, i, n = 60) {
+function didYouMean(e, i, n = 60) {  // Similarity ( Default: 60% (recommended) )
     return i.map(i => ({
         e: i, v: checkSimilarity(e, i) // checkSimilarity function can be found in stringSimilarity.js
     })).filter(({ v: e }) => e >= n / 100).reduce((e, i, n, r) => r[n].v > i ? r[n].v : i.e, null);
